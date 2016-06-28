8ea70ab79e8280afadfe5e2652fa852abce88432
Jarmo Jomppanen <tdatda@gmail.com>
2016-06-28 20:23:00 +0300

    Configure webpack entry for src/index.js

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
