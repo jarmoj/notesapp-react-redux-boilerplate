565ffbd840ee243549e5e2826a6ece2630b6f9a9
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-01 17:01:09 +0300

    Add react-split-pane and modify page CSS

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import NotesList from './NotesList'
 import NotesSearch from './NotesSearch'
+import SplitPane from "../../node_modules/react-split-pane/lib/SplitPane";
 
 export default class NotesApp extends React.Component {
   render() {
-    return <div>
+    return <div className="root">
       <section className="notes-app">
         <NotesSearch/>
-        <NotesList notes={this.props.notes} />
+        <SplitPane split="horizontal" defaultSize="50%">
+          <NotesList notes={this.props.notes} />
+          <div>Editor</div>
+        </SplitPane>
       </section>
     </div>
   }
