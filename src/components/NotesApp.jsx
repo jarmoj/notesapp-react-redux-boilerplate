c9cb1ac65c0f219f04e6d4f2cfdacdd8a2c807c6
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-05 10:28:53 +0300

    Implement notes search change handler and app search callback

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -3,17 +3,35 @@ import NotesList from './NotesList'
 import NotesSearch from './NotesSearch'
 import SplitPane from "../../node_modules/react-split-pane/lib/SplitPane";
 import NotesEdit from './NotesEdit'
+import {List, Map} from 'immutable';
+
 
 export default class NotesApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: List.of(),
+      edition: undefined
+    }
+  }
   getNote(id) {
     return this.props.notes.find(function(note) {
       return note.get('id') == id;
     });
   }
+  notesSearch(notes) {
+    if (!notes.length)
+      return;
+
+    this.setState({
+      notes: notes,
+      editing: notes[0].get('id')
+    });
+  }
   render() {
     return (
       <div className="notes-app">
-        <NotesSearch/>
+        <NotesSearch onSearchDone={this.notesSearch.bind(this)}/>
         <div className="contain-absolute">
           <SplitPane split="horizontal" defaultSize="50%">
             <NotesList notes={this.props.notes} />
--- src/components/NotesSearch.jsx
+++ src/components/NotesSearch.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {List, Map} from 'immutable';
+
 
 export default class NotesSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: ""
+    };
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+  makeSearch(e) {
+    this.props.onSearchDone(List.of());
+    this.setState({value: e.target.value});
   }
   render() {
     return (
       <div className="notes-search-border">
-        <input className="notes-search" autoFocus={true} type="text" />
+        <input ref="input"
+            className="notes-search"
+            autoFocus={true}
+            type="text"
+            value={this.state.value}
+            onChange={this.makeSearch.bind(this)} />
       </div>
       );
   }
