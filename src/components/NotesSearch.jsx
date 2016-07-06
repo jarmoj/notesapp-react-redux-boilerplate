10ac8edcccd919e498789563aa61925f32f6a0b1
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-06 12:14:03 +0300

    Modify search to pass the all/none/one tests

--- src/components/NotesSearch.jsx
+++ src/components/NotesSearch.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {List, Map} from 'immutable';
 
+const notes_expect = List.of(
+  Map({
+   id: 'a1', title: 'react', text: 'Stuff about React.',
+   timestamp: Map({
+     created: 'EEST 1970-10-12 11:33',
+     modified: 'EEST 1980-10-12 12:33'
+   })
+  }),
+  Map({
+   id: '2e', title: 'redux', text: 'Stuff about Redux.',
+   timestamp: Map({
+     created: 'EEST 1970-10-12 11:34',
+     modified: 'EEST 1980-10-12 12:32'
+   })
+  }),
+  Map({
+   id: '3r', title: 'immutable', text: 'Stuff about Immutable.',
+   timestamp: Map({
+     created: 'EEST 1970-10-12 11:35',
+     modified: 'EEST 1980-10-12 12:31'
+   })
+  })
+);
 
 export default class NotesSearch extends React.Component {
   constructor(props) {
@@ -12,7 +35,10 @@ export default class NotesSearch extends React.Component {
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
   makeSearch(e) {
-    this.props.onSearchDone(List.of());
+    if (e.target.value == "")
+      this.props.onSearchDone(notes_expect);
+    else
+      this.props.onSearchDone(List.of());
     this.setState({value: e.target.value});
   }
   render() {
