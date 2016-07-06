92e6effbda1e21b898097ff2ee53390d74b30dc2
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-06 19:53:38 +0300

    Add app component tests

--- test/components/NotesApp_spec.jsx
+++ test/components/NotesApp_spec.jsx
@@ -6,7 +6,8 @@ import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
 const {renderIntoDocument,
-       scryRenderedDOMComponentsWithTag} = TestUtils;
+       scryRenderedDOMComponentsWithTag,
+       Simulate} = TestUtils;
 
 const notes = List.of(
   Map({
@@ -49,5 +50,44 @@ describe('NotesApp - Searching', () => {
   });
   it('search notes suitably should return one of the notes', () => {
     expect(component.makeSearch("react")).to.equal(List.of(notes.get(0)));
-  });  
+  });
+});
+
+describe('NotesApp - Selection', () => {
+  let component;
+  let app;
+  beforeEach(() => {
+    component = renderIntoDocument(
+      <NotesApp/>
+    );
+    app = scryRenderedDOMComponentsWithTag(component, 'div');
+  });
+  it('by default the notes app has no note selected', () => {
+    expect(component.state.editing).to.equal(undefined);
+  });
+  it('onSelect handler changes the currently selected note', () => {
+    component.onSelect(notes.get(0).get('id'));
+    expect(component.state.editing).to.equal(notes.get(0).get('id'));
+  });
+  it('pressing esc will call onKeyDown', () => {
+    let wasCalled = false;
+    component.onKeyDown = function (e) {
+      wasCalled = true;
+    }
+    Simulate.keyDown(app[0], {key: 'Esc', keyCode: 27, which: 27});
+    expect(wasCalled);
+  });
+  it('calling onKeyDown with esc event will deselect current selection', () => {
+    expect(notes.get(0).get('id')).to.not.equal(undefined);
+    component.onSelect(notes.get(0).get('id'));
+    expect(component.state.editing).to.equal(notes.get(0).get('id'));
+    document.onkeydown({key: 'Esc', keyCode: 27, which: 27});
+    expect(component.state.editing).to.equal(undefined);
+  });
+  it('calling onKeyDown with esc event will clear search input', () => {
+    component.onSelect(notes.get(0).get('id'));
+    ReactDOM.findDOMNode(component._search._input).value = "jkhiuh";
+    document.onkeydown({key: 'Esc', keyCode: 27, which: 27});
+    expect(component._search._input.value).to.equal("");
+  });
 });
