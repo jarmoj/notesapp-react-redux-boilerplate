c7ed0e8430a6e802ae700085a64b845b7aeaa787
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-11 20:48:04 +0300

    Implement noteCallback in note edit

--- src/components/NotesEdit.jsx
+++ src/components/NotesEdit.jsx
@@ -7,10 +7,15 @@ export default class NotesEdit extends React.Component {
     super(props);
     //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
+  onChangeCallback(e) {
+    this.props.noteEdited(e.target.value);
+  }
   render() {
     return (
       <div className="notes-edit-border">
-        <textarea className="notes-edit"/>
+        <textarea className="notes-edit"
+                  ref={(c) => this._input = c}
+                  onChange={this.onChangeCallback.bind(this)} />
       </div>
     );
   }
