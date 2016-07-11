96eef328de550044b535d166135eb4bc7062626f
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-11 21:00:43 +0300

    Implement rowClicked callback in notes list item

--- src/components/NotesListItem.jsx
+++ src/components/NotesListItem.jsx
@@ -6,9 +6,14 @@ export default class NotesListItem extends React.Component {
     super(props);
     //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
+  onClickCallback(e) {
+    this.props.rowClicked(this.props.title);
+  }
   render() {
     return (
-      <tr className="notes-list-row">
+      <tr className="notes-list-row"
+          onClick={this.onClickCallback.bind(this)}
+          ref={(c) => this._row = c}>
         <td className="notes-list-item-title">
           title
           <span className="notes-list-item-text">
