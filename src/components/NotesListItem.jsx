615e8925c50b2be4522720c4023e10fcae282936
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-13 01:44:59 +0300

    Implement notes list item for its contents

--- src/components/NotesListItem.jsx
+++ src/components/NotesListItem.jsx
@@ -19,19 +19,25 @@ export default class NotesListItem extends React.Component {
     }
     return " even-row";
   }
+  timestamp() {
+    if ('timestamp' in this.props && 'orderBy' in this.props) {
+        return this.props.timestamp.get(this.props.orderBy)
+    }
+    return "";
+  }
   render() {
     return (
       <tr className={"notes-list-row" + this.oddOrEvenRow()}
           onClick={this.onClickCallback.bind(this)}
           ref={(c) => this._row = c}>
         <td className="notes-list-item-title">
-          title
+          {this.props.title}
           <span className="notes-list-item-text">
-            {' '} - text
+            {' '} - {this.props.text}
           </span>
         </td>
         <td className="notes-list-item-date">
-          timestamp
+          {this.timestamp()}
         </td>
         <td className="notes-list-item-destroy">
           <button className="destroy">X</button>
