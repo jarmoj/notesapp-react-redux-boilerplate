d7e0479b3eca4ce247830f9b1902a34f95edea72
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-13 01:44:45 +0300

    Implement test to check notes list item contents

--- test/components/NotesListItem_spec.jsx
+++ test/components/NotesListItem_spec.jsx
@@ -40,8 +40,29 @@ describe('NotesListItem - Visual', () => {
     expect(componentOdd._row.classList.contains('even-row')).to.equal(false);
     expect(componentOdd._row.classList.contains('odd-row')).to.equal(true);
   });
-  it('renders a list item with its title contents, beginning of text body, and date modified/created depending orderBy', () => {
-    expect(false).to.equal(true);
+  it('renders a list item with its title contents, text body, and date modified/created depending orderBy', () => {
+    const title = "some title title";
+    const text = "something for the text body";
+    const timestamp = Map({
+      created: 'EEST 1970-10-12 11:33',
+      modified: 'EEST 1980-10-12 12:33'
+    });
+
+    const componentModified = renderIntoDocument(
+      <NotesListItem title={title} text={text} timestamp={timestamp} orderBy="modified" />
+    );
+
+    expect(componentModified._row.textContent).to.contain(title);
+    expect(componentModified._row.textContent).to.contain(text);
+    expect(componentModified._row.textContent).to.contain(timestamp.get('modified'));
+
+    const componentCreated = renderIntoDocument(
+      <NotesListItem title={title} text={text} timestamp={timestamp} orderBy="created" />
+    );
+
+    expect(componentCreated._row.textContent).to.contain(title);
+    expect(componentCreated._row.textContent).to.contain(text);
+    expect(componentCreated._row.textContent).to.contain(timestamp.get('created'));
   });
   it('check that a list item that is selected by select prop gets selected class', () => {
     expect(false).to.equal(true);
