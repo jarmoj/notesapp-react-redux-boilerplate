b1147a7893bc2d4e989577b3c56ff263c3d32c13
Jarmo Jomppanen <tdatda@gmail.com>
2016-07-19 13:10:54 +0300

    Refactor test to expect plain actions

--- test/actions/index_spec.js
+++ test/actions/index_spec.js
@@ -13,19 +13,19 @@ import * as tk from 'timekeeper';
 describe('actions', () => {
   it('should create an action to set the complete state of the app', () => {
     const state = _state;
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.SET_STATE,
       state
-    });
+    };
     expect(actions.setState(state)).to.equal(expectedAction);
   });
 
   it('should create an action to set the search query', () => {
     const query = 'test query';
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.SET_QUERY,
       query
-    });
+    };
     expect(actions.setQuery(query)).to.equal(expectedAction);
   });
 
@@ -34,22 +34,22 @@ describe('actions', () => {
     tk.freeze(timestamp);
     const title = 'test title';
     const text = 'test content for note text';
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.ADD_NOTE,
       title,
       text,
       timestamp
-    });
+    };
     expect(actions.addNote(title, text)).to.equal(expectedAction);
     tk.reset();
   });
 
   it('should create an action to select note by title', () => {
     const title = 'test title';
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.SELECT_NOTE,
       title
-    });
+    };
     expect(actions.selectNote(title)).to.equal(expectedAction);
   });
 
@@ -59,30 +59,30 @@ describe('actions', () => {
     const selected = 'test title';
     const title = 'test title new';
     const text = 'test content for note text';
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.EDIT_NOTE,
       selected,
       title,
       text,
       timestamp
-    });
+    };
     expect(actions.editNote(selected, title, text)).to.equal(expectedAction);
     tk.reset();
   });
 
   it('should create an action to delete note with given title', () => {
     const selected = 'test title';
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.DELETE_NOTE,
       selected
-    });
+    };
     expect(actions.deleteNote(selected)).to.equal(expectedAction);
   });
 
   it('should create an action to toggle list order between modified and created', () => {
-    const expectedAction = Map({
+    const expectedAction = {
       type: types.TOGGLE_ORDER_BY
-    });
+    };
     expect(actions.toggleOrderBy()).to.equal(expectedAction);
   });
 });
