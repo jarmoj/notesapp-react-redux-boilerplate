d4120265dccd5af42da54633cdc7f7df2ecc7080
Jarmo Jomppanen <tdatda@gmail.com>
2016-08-01 00:37:00 +0300

    Implement note edit callback

--- src/components/NotesEdit.jsx
+++ src/components/NotesEdit.jsx
@@ -6,25 +6,40 @@ export default class NotesEdit extends React.Component {
   constructor(props) {
     super(props);
     //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state = this.stateValueFromProps(props);
+  }
+  componentWillReceiveProps(props) {
+    this.setState(this.stateValueFromProps(props));
+  }
+  stateValueFromProps(props) {
+    if (props.selected === undefined || !props.selected) {
+      return { value: "" };
+    }
+
+    const noteIndex = props.notes.findIndex(note => note.get("title") == props.selected);
+    const note = props.notes.get(noteIndex);
+    return { value: note.get("text") };
   }
   onChangeCallback(e) {
+    this.setState({ value: e.target.value });
     this.props.noteEdited(e.target.value);
   }
   isDisabled() {
-    if (!('selected' in this.props)) {
+    if (this.props.selected === undefined || !this.props.selected) {
       return ' disabled';
     }
 
-    if (!this.props.selected) {
-      return ' disabled';
-    }
     return '';
+  }
+  focus() {
+    this._textarea.focus();
   }
   render() {
     return (
       <div className="notes-edit-border">
         <textarea className={"notes-edit" + this.isDisabled()}
                   ref={(c) => this._textarea = c}
+                  value={this.state.value}
                   onChange={this.onChangeCallback.bind(this)}/>
       </div>
     );
