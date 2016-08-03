96964b3b9c0968331fbc86ba305341d44454526d
Jarmo Jomppanen <tdatda@gmail.com>
2016-08-03 05:09:25 +0300

    Make test data timestamps conform to chosen format

--- test/test_data.js
+++ test/test_data.js
@@ -38,27 +38,27 @@ export default Map({
               title: 'react',
               text: 'Stuff about React.',
               timestamp: Map({
-                created: 'EEST 1970-10-12 11:33',
-                modified: 'EEST 1980-10-12 12:33'
+                created: '1970-10-12T11:33:00.000Z',
+                modified: '1980-10-12T12:33:00.000Z'
               })
             }),
             Map({
               title: 'redux',
               text: 'Stuff about Redux.',
               timestamp: Map({
-                created: 'EEST 1970-10-12 11:34',
-                modified: 'EEST 1980-10-12 12:32'
+                created: '1970-10-12T11:34:00.000Z',
+                modified: '1980-10-12T12:32:00.000Z'
               })
             }),
             Map({
               title: 'immutable',
               text: 'Stuff about Immutable.',
               timestamp: Map({
-                created: 'EEST 1970-10-12 11:35',
-                modified: 'EEST 1980-10-12 12:31'
+                created: '1970-10-12T11:35:00.000Z',
+                modified: '1980-10-12T12:31:00.000Z'
               })
             })
           ),
   selected: null,
-  orderBy: 'modified descending'
+  orderBy: 'title ascending'
 });
